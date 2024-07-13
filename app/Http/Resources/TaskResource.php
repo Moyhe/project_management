abba7869c2e39e622830c2786749a829eca0bccb
Mohye <dragonsinmeliodas777@gmail.com>
2024-07-13 21:09:03 +0300

    add task api resource

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'image' => !(str_starts_with($this->image, 'https')) ?
+                Storage::url($this->image) : $this->image,
+            'project_id' => $this->project_id,
+            'project' => new ProjectResource($this->project),
+            'assigned_user_id' => $this->assigned_user_id,
+            'assignedUser' => $this->assignedUser ? new UserResource($this->assignedUser) : null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+        ];
+    }
+}
