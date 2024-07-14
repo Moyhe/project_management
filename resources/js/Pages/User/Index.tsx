9488330359af8347a6a06ceecf369cfdc6a60eaf
Mohye <dragonsinmeliodas777@gmail.com>
2024-07-14 05:28:02 +0300

    manage crud operations for  users

---
+++ resources/js/Pages/User/Index.tsx
@@ -0,0 +1,252 @@
+import Pagination from "@/Components/Pagination";
+import TableHeading from "@/Components/TableHeading";
+import TextInput from "@/Components/TextInput";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Users } from "@/types";
+import { QueryParams } from "@/types/queryParams";
+import { Head, Link, router } from "@inertiajs/react";
+import { useEffect, useRef, useState } from "react";
+
+interface Props {
+    users: Users;
+    queryParams: QueryParams;
+    success: string;
+}
+
+const index = ({ users, queryParams, success }: Props) => {
+    queryParams = queryParams || {};
+    const searchFiledInput = (
+        name: keyof typeof queryParams,
+        value: string
+    ) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+
+        router.get(route("user.index"), queryParams as {});
+    };
+
+    const search = (name: keyof typeof queryParams, event: any) => {
+        if (event.key !== "Enter") return;
+
+        searchFiledInput(name, event.target.value);
+    };
+
+    const sortChanged = (name: string) => {
+        if (name === queryParams.sort_field) {
+            if (queryParams.sort_direction === "asc") {
+                queryParams.sort_direction = "desc";
+            } else {
+                queryParams.sort_direction = "asc";
+            }
+        } else {
+            queryParams.sort_field = name;
+            queryParams.sort_direction = "asc";
+        }
+        router.get(route("user.index"), queryParams as {});
+    };
+
+    const deleteUser = (projectId: number) => {
+        if (!window.confirm("Are you sure you want to delete the project?")) {
+            return;
+        }
+        router.delete(route("user.destroy", projectId));
+    };
+
+    const ref = useRef(null);
+
+    const [localSuccess, setSuccess] = useState("");
+
+    useEffect(() => {
+        setSuccess(success);
+        setTimeout(() => {
+            if (ref.current) {
+                ref.current;
+            }
+        }, 3000);
+    }, [success]);
+
+    return (
+        <AuthenticatedLayout
+            header={
+                <div className="flex justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        Users
+                    </h2>
+                    <Link
+                        href={route("user.create")}
+                        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+                    >
+                        Add new
+                    </Link>
+                </div>
+            }
+        >
+            <Head title="Users" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {localSuccess && (
+                        <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+                            {localSuccess}
+                        </div>
+                    )}
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <div className="overflow-auto">
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <TableHeading
+                                                name="id"
+                                                sort_field={
+                                                    queryParams.sort_field
+                                                }
+                                                sort_direction={
+                                                    queryParams.sort_direction
+                                                }
+                                                sortChanged={sortChanged}
+                                            >
+                                                ID
+                                            </TableHeading>
+                                            <TableHeading
+                                                name="name"
+                                                sort_field={
+                                                    queryParams.sort_field
+                                                }
+                                                sort_direction={
+                                                    queryParams.sort_direction
+                                                }
+                                                sortChanged={sortChanged}
+                                            >
+                                                Name
+                                            </TableHeading>
+
+                                            <TableHeading
+                                                name="email"
+                                                sort_field={
+                                                    queryParams.sort_field
+                                                }
+                                                sort_direction={
+                                                    queryParams.sort_direction
+                                                }
+                                                sortChanged={sortChanged}
+                                            >
+                                                Email
+                                            </TableHeading>
+
+                                            <TableHeading
+                                                name="created_at"
+                                                sort_field={
+                                                    queryParams.sort_field
+                                                }
+                                                sort_direction={
+                                                    queryParams.sort_direction
+                                                }
+                                                sortChanged={sortChanged}
+                                            >
+                                                Create Date
+                                            </TableHeading>
+
+                                            <th className="px-3 py-3 text-right">
+                                                Actions
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3">
+                                                <TextInput
+                                                    className="w-full"
+                                                    defaultValue={
+                                                        queryParams.name
+                                                    }
+                                                    placeholder="User Name"
+                                                    onBlur={(e) =>
+                                                        searchFiledInput(
+                                                            "name",
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    onKeyUp={(e) =>
+                                                        search("name", e)
+                                                    }
+                                                />
+                                            </th>
+                                            <th className="px-3 py-3">
+                                                <TextInput
+                                                    className="w-full"
+                                                    defaultValue={
+                                                        queryParams.email
+                                                    }
+                                                    placeholder="User Email"
+                                                    onBlur={(e) =>
+                                                        searchFiledInput(
+                                                            "email",
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    onKeyUp={(e) =>
+                                                        search("email", e)
+                                                    }
+                                                />
+                                            </th>
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {users.data.map((user) => (
+                                            <tr
+                                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                                key={user.id}
+                                            >
+                                                <td className="px-3 py-2">
+                                                    {user.id}
+                                                </td>
+                                                <th className="px-3 py-2 text-gray-100 text-nowrap">
+                                                    {user.name}
+                                                </th>
+                                                <td className="px-3 py-2">
+                                                    {user.email}
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    {user.created_at}
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    <Link
+                                                        href={route(
+                                                            "user.edit",
+                                                            user.id
+                                                        )}
+                                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <button
+                                                        onClick={(e) =>
+                                                            deleteUser(user.id)
+                                                        }
+                                                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                            <Pagination links={users.meta.links} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+};
+
+export default index;
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,14 +1,23 @@
+import { Links } from "./project";
+
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
+  created_at: string;
+}
+
+export interface Users {
+  data: User[];
+  meta: { links: Links[] };
+  links: {};
 }
 
 export type PageProps<
-    T extends Record<string, unknown> = Record<string, unknown>
+  T extends Record<string, unknown> = Record<string, unknown>
 > = T & {
-    auth: {
-        user: User;
-    };
+  auth: {
+    user: User;
+  };
 };
